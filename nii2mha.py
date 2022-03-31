bb40a348db376e552ffbe7c4473e09b3f923ec60
sergiosgatidis <52936169+sergiosgatidis@users.noreply.github.com>
2022-03-31 09:06:20 +0200

    Update nii2mha.py

--- nii2mha.py
+++ nii2mha.py
@@ -1,3 +1,9 @@
+# converts the entire dataset from the .nii.gz format to the .mha format
+#(the .mha format is required by grand-challenge.org as input and ouput data of algorithms)
+
+# inputs:
+# input 1: path_to_nii_data : path to nifti data e.g.  .../nifti/FDG-PET-CT-Lesions/
+# input 2: path_to_mha_data : output path for mha data ... /mha/FDG-PET-CT-Lesions/ (will be created if non existing)
 
 import SimpleITK as sitk
 import pathlib as plb
@@ -5,7 +11,7 @@ import tqdm
 import os
 import sys
 
-def find_studies(path_to_data):
+def find_studies(path_to_data):  # returns a list of unique study paths within the dataset
     dicom_root = plb.Path(path_to_data)
     patient_dirs = list(dicom_root.glob('*'))
 
@@ -19,12 +25,12 @@ def find_studies(path_to_data):
         #dicom_dirs = dicom_dirs.append(dir.glob('*'))
     return study_dirs
 
-def nii_to_mha(nii_path, mha_out_path):
+def nii_to_mha(nii_path, mha_out_path): # converts a .nii.gz file to .mha and saves to a specified path
     img = sitk.ReadImage(nii_path)
     sitk.WriteImage(img, mha_out_path, True)
 
 
-def convert_to_mha(study_dirs,path_to_mha_data):
+def convert_to_mha(study_dirs,path_to_mha_data): # main function converting the entire dataset from .nii.gz to .mha
         
     for study_dir in tqdm.tqdm(study_dirs):
 
@@ -37,23 +43,23 @@ def convert_to_mha(study_dirs,path_to_mha_data):
         pet_nii    = str(study_dir/'PET.nii.gz')
         seg_nii    = str(study_dir/'SEG.nii.gz')
 
-        suv_mha    = os.path.join(path_to_mha_data, patient, study)
-        ctres_mha  = os.path.join(path_to_mha_data, patient, study)
-        ct_mha     = os.path.join(path_to_mha_data, patient, study)
-        pet_mha    = os.path.join(path_to_mha_data, patient, study)
-        seg_mha    = os.path.join(path_to_mha_data, patient, study)
+        suv_mha_dir    = os.path.join(path_to_mha_data, patient, study)
+        ctres_mha_dir  = os.path.join(path_to_mha_data, patient, study)
+        ct_mha_dir     = os.path.join(path_to_mha_data, patient, study)
+        pet_mha_dir    = os.path.join(path_to_mha_data, patient, study)
+        seg_mha_dir    = os.path.join(path_to_mha_data, patient, study)
 
-        os.makedirs(suv_mha  , exist_ok=True)
-        os.makedirs(ctres_mha, exist_ok=True)
-        os.makedirs(ct_mha   , exist_ok=True)
-        os.makedirs(pet_mha  , exist_ok=True)
-        os.makedirs(seg_mha  , exist_ok=True)
+        os.makedirs(suv_mha_dir  , exist_ok=True)
+        os.makedirs(ctres_mha_dir, exist_ok=True)
+        os.makedirs(ct_mha_dir   , exist_ok=True)
+        os.makedirs(pet_mha_dir  , exist_ok=True)
+        os.makedirs(seg_mha_dir  , exist_ok=True)
 
-        nii_to_mha(suv_nii,   os.path.join(suv_mha,'SUV.mha'))
-        nii_to_mha(ctres_nii, os.path.join(ctres_mha,'CTres.mha'))
-        nii_to_mha(ct_nii,    os.path.join(ct_mha,'CT.mha'))
-        nii_to_mha(pet_nii,   os.path.join(pet_mha,'PET.mha'))
-        nii_to_mha(seg_nii,   os.path.join(seg_mha,'SEG.mha') )     
+        nii_to_mha(suv_nii,   os.path.join(suv_mha_dir,'SUV.mha'))
+        nii_to_mha(ctres_nii, os.path.join(ctres_mha_dir,'CTres.mha'))
+        nii_to_mha(ct_nii,    os.path.join(ct_mha_dir,'CT.mha'))
+        nii_to_mha(pet_nii,   os.path.join(pet_mha_dir,'PET.mha'))
+        nii_to_mha(seg_nii,   os.path.join(seg_mha_dir,'SEG.mha') )     
 
 
 if __name__ == "__main__":
